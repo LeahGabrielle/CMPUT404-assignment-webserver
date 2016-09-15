978ffbf69d173d0839c9362ae67fedbbde49afe1
LeahGabrielle <olexson@ualberta.ca>
2016-09-15 13:53:11 -0600

    i pass some tests

--- server.py
+++ server.py
@@ -30,9 +30,16 @@ import mimetypes
 class MyWebServer(SocketServer.BaseRequestHandler):
     
     def handle(self):
+        self.success = True
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
 	self.parse_request(self.data)
+        if (self.success):
+            #send 200 message
+            self.request.sendall("HTTP/1.1 200 OK\r\n")
+            #http://stackoverflow.com/questions/21153262/sending-html-through-python-socket-server
+            print self.content_type
+            self.request.sendall("Content-Type:"+self.content_type+"\r\n")
 
     def hasExtension(self):
         try:
@@ -40,6 +47,7 @@ class MyWebServer(SocketServer.BaseRequestHandler):
             self.request.sendall(f.read())
             f.close
         except:
+           print "HERE I AM"
            self.error_404()
 
     def noExtension(self, ext):
@@ -55,48 +63,38 @@ class MyWebServer(SocketServer.BaseRequestHandler):
             f = open('www/'+self.path+"index.html")
             self.request.sendall(f.read())
             f.close
+            self.content_type = "text/html"
         except:
             self.error_404()
 
-# http://www.acmesystems.it/python_httpd
     def endType(self):
     	reply = False
-        print "PATH: "+self.path
+        #print "PATH: "+self.path
         extension = mimetypes.guess_type(self.path)[0]
-        end = self.path.split(".")
-        print extension
+        #end = self.path.split(".")
+        #print extension
         if (extension == None):
             #This means no .html, .css
             if (self.path == "" or self.path[-1] == "/"): 
                 self.directory()
             else:
-                if (self.acceptTypes[0] == "text/html"):
+                #if (self.acceptTypes[0] == "text/html"):
+                #print self.path[-5::]
+                if (self.path[-5::] == "index"):
                     extension = '.html'
-                elif (self.acceptTypes[0] == "text/css"):
+                    self.content_type = "text/html"
+                #elif (self.acceptTypes[0] == "text/css"):
+                elif (self.path[-5::] == "index"):
                     extension = '.css'
+                    self.content_type = "text/css"
                 else:
                     self.error_404()
             
                 self.noExtension(extension)
         else: # we got the extension from guess_type
+            self.content_type = extension
             self.hasExtension()
-        
-        '''
-    	if (self.acceptTypes[0] == "text/html"):
-	   mimetype = 'text/html'
-	   extension = '.html'
-	   reply = True
-	elif (self.acceptTypes[0] == "text/css"):
-	   mimetype = 'text/css'
-           extension = '.css'
-	   reply = True;
-        if reply:
-            print end
-            if (len(end) == 1):
-                self.noExtension(extension)
-            else:
-                self.hasExtension()
-        '''
+
 #from sberry at http://stackoverflow.com/questions/18563664/socketserver-python 
     def parse_request(self, req):
         headers = {}
@@ -112,7 +110,7 @@ class MyWebServer(SocketServer.BaseRequestHandler):
                 k, v = line.split(":", 1)
                 headers[k.strip()] = v.strip()
         method, path, _ = lines[0].split()
-	self.acceptTypes = headers.get('Accept').split(",")
+        #self.acceptTypes = headers.get('Accept').split(",")
 	#print self.acceptTypes
         self.path = path.lstrip("/")
         self.method = method
@@ -120,14 +118,15 @@ class MyWebServer(SocketServer.BaseRequestHandler):
         self.body = body
 	#print self.headers
 	#print self.path
-	print "parse self.path: " + self.path
+	#print "parse self.path: " + self.path
 	#print self.method
 	#print self.body	
 	
 	self.endType()	
 
     def error_404(self):
-        self.request.sendall("Error 404")
+        self.request.sendall("HTTP/1.1 404 NOT FOUND\r\n\r\n")
+        self.success = False
     
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
